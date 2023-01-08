f7f165405ee0ccb1248ad2bf3da723aa8e83715d
okazaki1224 <guangcheng1224@gmail.com>
2023-01-08 07:08:35 +0000

    [Add]ｴﾗｰ回避、ﾀﾞﾐｰ画像、devise、ActiveStorage、管理・会員ﾓﾃﾞﾙ、ﾃｰﾌﾞﾙ設定、管理・会員ｺﾝﾄﾛｰﾗ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "294c53f2fa1640288f18f49e3b4382b2.vfs.cloud9.us-east-1.amazonaws.com"
+
+  config.active_job.queue_adapter = :inline
 end
