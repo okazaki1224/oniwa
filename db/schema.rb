973678aa30e3605f083bccee06d158b694c4bc27
okazaki1224 <guangcheng1224@gmail.com>
2023-01-08 07:19:50 +0000

    [Add]introductionｶﾗﾑを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_08_064210) do
+ActiveRecord::Schema.define(version: 2023_01_08_071636) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2023_01_08_064210) do
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "nickname", null: false
+    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
