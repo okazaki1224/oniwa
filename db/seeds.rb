7c0d58e1c68d9539cd6bdb059ac830fc8cc5d399
okazaki1224 <guangcheng1224@gmail.com>
2023-01-11 16:04:07 +0000

    [Test]devise.jaを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-  email: 'momiji@momiji',
-  password: 'momijimomiji',
+  email: 'izumi@izumi',
+  password: 'youmetown',
 )
